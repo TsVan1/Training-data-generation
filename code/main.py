08ef6f560d3721147c02239e612938daa5910103
Van <92656983+TsVan1@users.noreply.github.com>
2023-09-02 03:15:16 +0800

    更改数据格式
    更改数据格式

--- code/main.py
+++ code/main.py
@@ -4,10 +4,11 @@ import os
 
 
 def get_file_names_in_folder(folder_path):
-    file_names = []
+    file_names = {}
     for filename in os.listdir(folder_path):
         if os.path.isfile(os.path.join(folder_path, filename)):
-            file_names.append(filename)
+            file_name_without_extension = os.path.splitext(filename)[0]
+            file_names[file_name_without_extension] = filename
     return file_names
 
 
@@ -16,7 +17,7 @@ object_path = "../object/"
 background_path = "../background/"
 output_folder_path = "../DataOut/"
 
-# 获取待选图片列表
+# 获取待选图片列表，使用字典映射文件名到图片名称去掉后缀
 candidate_images = get_file_names_in_folder(object_path)
 
 # 生成图像的数量
@@ -34,19 +35,23 @@ object_height = 100
 if not os.path.exists(output_folder_path):
     os.makedirs(output_folder_path)
 
+# 生成文件名到数字的映射字典
+file_name_to_number_mapping = {name: i for i, name in enumerate(candidate_images)}
+
 # 生成图像并保存
 for i in range(num_images):
     background = Image.open(background_path + "background.png").resize((image_width, image_height))
     draw = ImageDraw.Draw(background)
 
-    selected_candidates = random.sample(candidate_images, 10)
+    selected_candidates = random.sample(list(candidate_images.keys()), 10)
 
     txt_data = []
 
     for candidate in selected_candidates:
         x = random.randint(0, image_width - object_width)
         y = random.randint(0, image_height - object_height)
-        candidate_image = Image.open(os.path.join(object_path, candidate)).resize((object_width, object_height))
+        candidate_image = Image.open(os.path.join(object_path, candidate_images[candidate])).resize(
+            (object_width, object_height))
         background.paste(candidate_image, (x, y))
 
         # 计算归一化坐标并保留小数点后5位
@@ -55,11 +60,16 @@ for i in range(num_images):
         width_normalized = round(object_width / image_width, 5)
         height_normalized = round(object_height / image_height, 5)
 
-        class_name = os.path.splitext(candidate)[0]  # 去除扩展名
+        class_number = file_name_to_number_mapping[candidate]  # 使用数字作为类别
         txt_data.append(
-            f"{class_name} {x_normalized:.5f} {y_normalized:.5f} {width_normalized:.5f} {height_normalized:.5f}")
+            f"{class_number} {x_normalized:.5f} {y_normalized:.5f} {width_normalized:.5f} {height_normalized:.5f}")
 
-    background.save(os.path.join(output_folder_path, f"image_{i}.png"))
+    background.save(os.path.join(output_folder_path, f"image_{i}.jpg"))
 
     with open(os.path.join(output_folder_path, f"image_{i}.txt"), "w") as txt_file:
         txt_file.write("\n".join(txt_data))
+
+# 生成类别名称到数字的映射关系txt文件
+with open(os.path.join(output_folder_path, "class_mapping.txt"), "w") as mapping_file:
+    for name, number in file_name_to_number_mapping.items():
+        mapping_file.write(f"{number}:{name}\n")
