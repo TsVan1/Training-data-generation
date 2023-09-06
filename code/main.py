90c815c756e54c0b0991ee5a6b77451aee3fac29
Van <92656983+TsVan1@users.noreply.github.com>
2023-09-06 17:35:16 +0800

    按照训练，验证8：2生成数据并保存不同目录
    按照训练，验证8：2生成数据并保存不同目录

--- code/main.py
+++ code/main.py
@@ -1,19 +1,21 @@
 import random
 from PIL import Image, ImageDraw, ImageOps
 import os
+import shutil
 
 # 配置参数
 object_path = "../object/"
 background_path = "../background/"
-output_folder_path = "../DataOut/"
+output_root = "../DataOut/"
 num_images = 100
 image_size = (1024, 1024)
 object_size = (100, 100)
 border_width = 5
 
-# 检查输出文件夹是否存在，不存在则创建
-if not os.path.exists(output_folder_path):
-    os.makedirs(output_folder_path)
+# 检查输出根目录是否存在，不存在则创建
+if not os.path.exists(output_root):
+    os.makedirs(output_root)
+
 
 # 获取待选图片列表
 def get_file_names_in_folder(folder_path):
@@ -24,10 +26,12 @@ def get_file_names_in_folder(folder_path):
             file_names[file_name_without_extension] = filename
     return file_names
 
+
 candidate_images = get_file_names_in_folder(object_path)
 
 # 创建文件名到数字的映射字典
 file_name_to_number_mapping = {name: i for i, name in enumerate(candidate_images)}
+
 
 # 函数：将对象裁剪为圆形
 def crop_to_circle(image):
@@ -38,15 +42,33 @@ def crop_to_circle(image):
     circle_image.paste(image, mask=mask)
     return circle_image
 
+
 # 函数：在对象位置绘制圆形边框
 def draw_circular_border(image, border_color):
     border_image = Image.new("RGBA", image.size, (0, 0, 0, 0))
     draw = ImageDraw.Draw(border_image)
     center_x, center_y = image.size[0] // 2, image.size[1] // 2
     radius = image.size[0] // 2
-    draw.ellipse((center_x - radius, center_y - radius, center_x + radius, center_y + radius), outline=border_color, width=border_width)
+    draw.ellipse((center_x - radius, center_y - radius, center_x + radius, center_y + radius), outline=border_color,
+                 width=border_width)
     result = Image.alpha_composite(image.convert("RGBA"), border_image)
     return result
+
+
+# 计算80%和20%的数据量
+num_train_images = int(num_images * 0.8)
+num_val_images = num_images - num_train_images
+
+# 创建训练和验证目录
+train_images_dir = os.path.join(output_root, "images", "train001")
+val_images_dir = os.path.join(output_root, "images", "val001")
+train_labels_dir = os.path.join(output_root, "labels", "train001")
+val_labels_dir = os.path.join(output_root, "labels", "val001")
+
+os.makedirs(train_images_dir, exist_ok=True)
+os.makedirs(val_images_dir, exist_ok=True)
+os.makedirs(train_labels_dir, exist_ok=True)
+os.makedirs(val_labels_dir, exist_ok=True)
 
 # 生成图像并保存
 for i in range(num_images):
@@ -63,7 +85,8 @@ for i in range(num_images):
         candidate_image = Image.open(os.path.join(object_path, candidate_images[candidate])).convert("RGBA")
         candidate_image = crop_to_circle(candidate_image)
 
-        border_color = (random.randint(0, 255), 0, 0, 255) if random.random() < 0.5 else (0, 0, random.randint(0, 255), 255)
+        border_color = (random.randint(0, 255), 0, 0, 255) if random.random() < 0.5 else (
+        0, 0, random.randint(0, 255), 255)
         candidate_image = draw_circular_border(candidate_image, border_color)
         candidate_image = candidate_image.resize(object_size)
 
@@ -78,12 +101,23 @@ for i in range(num_images):
         txt_data.append(
             f"{class_number} {x_normalized:.5f} {y_normalized:.5f} {width_normalized:.5f} {height_normalized:.5f}")
 
-    background.save(os.path.join(output_folder_path, f"image_{i}.jpg"))
+    # 生成文件名
+    file_name = f"image_{i:04d}"
 
-    with open(os.path.join(output_folder_path, f"image_{i}.txt"), "w") as txt_file:
+    # 决定是放入训练集还是验证集
+    if i < num_train_images:
+        image_save_path = os.path.join(train_images_dir, f"{file_name}.jpg")
+        label_save_path = os.path.join(train_labels_dir, f"{file_name}.txt")
+    else:
+        image_save_path = os.path.join(val_images_dir, f"{file_name}.jpg")
+        label_save_path = os.path.join(val_labels_dir, f"{file_name}.txt")
+
+    background.save(image_save_path)
+
+    with open(label_save_path, "w") as txt_file:
         txt_file.write("\n".join(txt_data))
 
 # 生成类别名称到数字的映射关系txt文件
-with open(os.path.join(output_folder_path, "class_mapping.txt"), "w") as mapping_file:
+with open(os.path.join(output_root, "class_mapping.txt"), "w") as mapping_file:
     for name, number in file_name_to_number_mapping.items():
         mapping_file.write(f"{number}: {name}\n")
